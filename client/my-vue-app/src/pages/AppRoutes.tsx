11912d39623d2753f46a9812dedcd0f0cbba3f62
gr0nn1x <fanyfany888@gmail.com>
2024-05-13 11:13:28 +0200

    approutes

--- client/my-vue-app/src/pages/AppRoutes.tsx
+++ client/my-vue-app/src/pages/AppRoutes.tsx
@@ -1,11 +1,17 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage/MainPage";
+import Post from "./Post/Post";
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/post" element={<Post />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
