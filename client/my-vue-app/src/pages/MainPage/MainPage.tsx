e1d3d389a9f93daabf4d6dc9b58b26b97f1e75d1
Bushak <jindra2004@seznam.cz>
2024-05-27 11:35:56 +0200

    awdawf

--- client/my-vue-app/src/pages/MainPage/MainPage.tsx
+++ client/my-vue-app/src/pages/MainPage/MainPage.tsx
@@ -1,23 +1,51 @@
-import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Navbar from '../Navbar/Navbar'; // Import the Navbar component
- 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
- 
-export default function MainPage() {
+import { Link } from "react-router-dom";
+import { getUploads } from "../../models/Post";
+import { useEffect, useState } from "react";
+import Article from "../../components/Product";
+import * as React from "react";
+
+export default function Home() {
+  const [uploads, setUploads] = useState();
+  const [isLoaded, setLoaded] = useState<any | null>(null);
+
+  const load = async () => {
+    const data = await getUploads();
+    console.log(data);
+    if (data.status === 200) {
+      setUploads(data.payload); 
+      setLoaded(true);
+    } else {
+      setLoaded(null);
+    }
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  if (isLoaded === null) {
+    return (
+      <>
+        <h1>Home page</h1>
+        <p>Images not found</p>
+        <Link to={"/upload"}>
+          <p>Upload new image</p>
+        </Link>
+      </>
+    );
+  }
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <Navbar/> {/* Use the Navbar component */}
-      </Box>
-    </ThemeProvider>
+    <>
+      <h1>Home page</h1>
+      {isLoaded ? (
+        uploads.map((upload, index) => <Article key={index} {...upload} />)
+      ) : (
+        <p>Loading</p>
+      )}
+      <Link to={"/createpost"}>
+        <p>Upload new image</p>
+      </Link>
+    </>
   );
 }
