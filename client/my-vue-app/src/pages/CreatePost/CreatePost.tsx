378b5224f3be7b5c9e15645734bb477d2331deff
MicolopoCZ <armymisa@gmail.com>
2024-06-11 08:21:47 +0200

    New createp Post

--- client/my-vue-app/src/pages/CreatePost/CreatePost.tsx
+++ client/my-vue-app/src/pages/CreatePost/CreatePost.tsx
@@ -2,6 +2,179 @@ import React, { useRef, useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import { createPost } from "../../models/Post";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Navbar from "../Navbar/Navbar";
+
+export default function CreatePost() {
+  const [info, setInfo] = useState();
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
+  const imgRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formDataToSend = new FormData();
+    for (const [key, value] of Object.entries(formData)) {
+      formDataToSend.append(key, value);
+    }
+    formDataToSend.append("photo", imgRef.current.files[0]);
+    console.log(...formDataToSend);
+    const post = await createPost(formDataToSend);
+
+    if (post.status === 201) return navigate("/");
+    if (post.status === 400) return setInfo(post.msg);
+    if (post.status === 500) return navigate("/");
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const defaultTheme = createTheme();
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Navbar title="Create Post" /> {/* Add Navbar component */}
+      <Container component="main" sx={{ marginLeft: '25%' }}> {/* Add paddingLeft */}
+        <CssBaseline />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Nový inzerát
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3, width: '100%' }} // Adjust width to 100%
+          >
+            <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "left",
+            }}
+            >
+            <Box>
+              <Grid container justifyContent="center"> {/* Center the grid container */}
+                <Grid item> {/* Wrap the file input and upload button in a Grid item */}
+                  <TextField type="file" name="photo" id="photo"/>
+                </Grid>
+              </Grid>
+
+              <Grid container>
+                <Grid xs justifyContent="center"> {/* Center the grid container */}
+                  <Grid item> {/* Wrap the file input and upload button in a Grid item */}
+                    <TextField type="file" name="photo" id="photo" />
+                  </Grid>
+                </Grid>
+                <Grid xs justifyContent="center"> {/* Center the grid container */}
+                  <Grid item> {/* Wrap the file input and upload button in a Grid item */}
+                    <TextField type="file" name="photo" id="photo" />
+                  </Grid>
+                </Grid>
+                <Grid xs justifyContent="center"> {/* Center the grid container */}
+                  <Grid item> {/* Wrap the file input and upload button in a Grid item */}
+                    <TextField type="file" name="photo" id="photo" />
+                  </Grid>
+                </Grid>
+                <Grid xs justifyContent="center"> {/* Center the grid container */}
+                  <Grid item> {/* Wrap the file input and upload button in a Grid item */}
+                    <TextField type="file" name="photo" id="photo" />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Box>
+            </Box>
+
+            <Grid container spacing={0}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="name"                     // předělat
+                  label="Jméno"
+                  type="name"                     // předělat
+                  id="name"                       // předělat
+                  autoComplete="given-name"       // předělat
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="phone"                   // předělat
+                  label="Telefon"
+                  type="phone"                   // předělat
+                  id="phone"                     // předělat
+                  autoComplete="given-phone"     // předělat
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="address"                   // předělat
+                  label="Adresa"
+                  type="address"                   // předělat
+                  id="address"                     // předělat
+                  autoComplete="given-address"     // předělat
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="price"                   // předělat
+                  label="Cena"
+                  type="price"                   // předělat
+                  id="price"                     // předělat
+                />
+              </Grid>
+            </Grid>
+            <Grid container xs={"auto"}>
+              <TextField
+                required
+                fullWidth
+                name="desc"                     // předělat
+                label="Popis"
+                type="desc"                     // předělat
+                id="desc"                       // předělat
+              />
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Zaslat inzerát
+            </Button>
+            <p>{info}</p>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+// stará verze
+/*
+import React, { useRef, useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import { createPost } from "../../models/Post";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -56,7 +229,6 @@ export default function CreatePost() {
         sx={{ marginLeft: "800px", paddingLeft: "16px" }}
       >
         {" "}
-        {/* Add paddingLeft */}
         <CssBaseline />
         <Box
           sx={{
@@ -160,3 +332,4 @@ export default function CreatePost() {
     </ThemeProvider>
   );
 }
+*/
